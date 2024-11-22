95255811731d5c1a91f51327a8ceef5e9be6bb55
Jukaro <jeremi1005@outlook.fr>
2024-11-22 17:04:20 +0100

    histogram

--- histogram.py
+++ histogram.py
@@ -1,16 +1,35 @@
 import matplotlib.pyplot as plt
-import numpy as np
+
+from classes.VisualizationData import VisualizationData
 
 def set_hist(plot, title, data):
     plot.hist(data, label=title, alpha=.5, edgecolor='red')
     plot.set_title(title)
 
-fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(nrows=2, ncols=2)
+data = VisualizationData("datasets/dataset_train.csv")
 
-set_hist(ax0, 'house1', np.random.normal(170, 10, 250))
-set_hist(ax1, 'house2', np.random.normal(170, 10, 250))
-set_hist(ax2, 'house3', np.random.normal(170, 10, 250))
-set_hist(ax3, 'house4', np.random.normal(170, 10, 250))
+fig, axs = plt.subplots(nrows=4, ncols=4, figsize=(15,15))
 
-fig.tight_layout()
-plt.show()
+courses = data.houses["Gryffindor"].keys()
+houses = data.houses.keys()
+
+cols_count = 4
+
+for i, course in enumerate(courses):
+    for house in houses:
+        axs[int(i / cols_count), i % cols_count].hist(data.houses[house][course], bins=100, alpha=0.5, label=house)
+        axs[int(i / cols_count), i % cols_count].set_title(course)
+
+fig.delaxes(axs[3, 1])
+fig.delaxes(axs[3, 2])
+fig.delaxes(axs[3, 3])
+
+for ax in axs.flat:
+    ax.set(xlabel='Grades', ylabel='Count')
+
+fig.tight_layout(pad=4.0)
+
+handles, labels = axs[0, 0].get_legend_handles_labels()
+fig.legend(handles, labels, loc='lower right', fontsize="20", bbox_to_anchor=(0.96, 0.04))
+
+fig.savefig("histogram.png")
